64eadf29f9a8eef59ab76d263db16f2ee6412f79
JPAIchatbot <nathan.hite@jprifles.com>
2024-11-12 11:38:10 -0600

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -117,7 +117,7 @@ app.post('/chat', async (req, res) => {
 
     // Call the OpenAI API for response generation
     const completion = await openai.chat.completions.create({
-      model: "ft:gpt-4o-2024-08-06:jp-enterprises:scs-modification:ASo1a6OG",
+      model: "ft:gpt-4o-2024-08-06:jp-enterprises:v7:ASp7NOUN",
       messages: conversationHistory,
       max_tokens: 256,
     });
